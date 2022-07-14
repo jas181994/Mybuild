3589cce195568aef51497ee6f49978aeb52c81ba
jas181994 <108090203+jas181994@users.noreply.github.com>
2022-07-14 16:03:12 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='Mypkg',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=['Mypkg'],
+zip_safe=True)
---
+++ test.py
@@ -0,0 +1,4 @@
+from Mypkg import power, average, SayHello
+SayHello(jasmeet")
+x=power(3,2)
+print("power(3,2) : ", x)
